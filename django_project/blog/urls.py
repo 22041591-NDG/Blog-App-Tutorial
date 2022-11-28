14a6704147a6ef6baf89014b4a63d0a0ea678e5e
22041591-NDG <limeynd@gmail.com>
2022-11-28 16:56:54 +0200

    Steps to Create,Update and Delete Posts

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,10 +1,21 @@
 from django.conf.urls import url
 from django.urls import path
+from .views import (PostListView,
+                    PostDetailView,
+                    PostCreateView,
+                    PostUpdateView,
+                    PostDeleteView)
 from . import views
 
 urlpatterns = [
     #url('', views.home, name = 'blog-home'),
-    path('', views.home, name = 'blog-home'),
+    #path('', views.home, name = 'blog-home'),
+    path('', PostListView.as_view(), name = 'blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name = 'post-detail'),
+    path('post/new/', PostCreateView.as_view(), name = 'post-create '),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name = 'post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name = 'post-delete'),
     path('about/', views.about, name='blog-about'),
-
 ]
+
+# <app>/<model>_<viewtype>.html
